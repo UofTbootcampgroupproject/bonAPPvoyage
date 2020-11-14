5638d3ff90a61f283620b5555a7d99aa7d1a5d01
Jhonny Lin <jhonnylin72@gmail.com>
2020-11-14 18:13:35 -0500

    Added jQuery to the index and did some changes on the script file
    added jQuery to the file. On the script added a ready function for when the site is ready and added the fetch for the triposo Api

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,5 @@
-
-
-
-// Modal Events
+$(document).ready(function () {
+    // Modal Events
 // var recipeButton = document.querySelector("#recipe");
 var instModalBg = document.querySelector("#inst-modal-background");
 var infoModalBg = document.querySelector("#info-modal-background");
@@ -17,6 +15,23 @@ instModalBg.addEventListener("click", function() {
 instModalCloseButton.addEventListener("click", function() {
     instModal.classList.remove("is-active");
 })
+    //Starting fetch request for spoonacular Api
+
+    //Starting fetch request for triposo
+var location = "Toronto";
+var triposoId = "98JDSPD1";
+var triposoApiKey = "opge12o7zdr1npc4primk2yaxn3omhxa";
+var triposoUrl = "https://www.triposo.com/api/20201111/poi.json?location_id="+ location + "&account="+ triposoId +"&token="+ triposoApiKey +"&count=4&fields=id,name,score,snippet,location_id,tag_labels&order_by=-score";
+fetch(triposoUrl)
+    .then(function(response) {
+        console.log("Triposo has a "+response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        return data;
+    })
+
 
 // recipeButton.addEventListener("click", function() {
 //     infoModal.classList.add("is-active");
@@ -29,3 +44,4 @@ instModalCloseButton.addEventListener("click", function() {
 // infoModalCloseButton.addEventListener("click", function() {
 //     infoModal.classList.remove("is-active");
 // })
+});
